d86c18f1ed29f31d4b3131b09d8185c8d05cff13
Markus Rothlübbers <markus@mrlb.space>
2025-03-20 20:23:05 +0100

    Remove development-only code from service worker

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Service worker for StoryPoint for Asana
-chrome.runtime.onInstalled.addListener(details => {
+chrome.runtime.onInstalled.addListener(function (details) {
   if (details.reason === 'install') {
     console.log('StoryPoint for Asana installed');
   } else if (details.reason === 'update') {
@@ -10,19 +10,3 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 
 console.log('StoryPoint for Asana service worker initialized');
-
-// Development reloading
-if (process.env.NODE_ENV === 'development') {
-  let lastReload = Date.now();
-  
-  // Listen for reload messages from development server
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'RELOAD_EXTENSION') {
-      const currentTime = Date.now();
-      if (currentTime - lastReload > 60000) { // Don't reload more than once a minute
-        lastReload = currentTime;
-        chrome.runtime.reload();
-      }
-    }
-  });
-}
